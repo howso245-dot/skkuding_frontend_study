9f718b15b29f29582770b8aecead6c04f8c8442c
howso245 <howso245@gmail.com>
2026-06-02 20:43:23 +0900

    ...

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/skkuding_frontend_study'
+  basePath: '/skkuding_frontend_study',
+  assetPrefix: '/skkuding_frontend_study/'
 };
 
 export default nextConfig;
